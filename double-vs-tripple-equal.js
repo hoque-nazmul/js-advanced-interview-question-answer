bd6898023771634a09aabd37f6c583a1f55ec8c6
Nazmul Hoque <coder.nazmul@gmail.com>
2020-03-04 01:10:09 +0600

    double vs tripple equal is cleared

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,18 @@
+let numOne = 2;
+let numTwo = "2";
+// == check only value but === check value and data type also.
+if(numOne == numTwo) {
+    console.log("Condition is true!");
+}
+else {
+    console.log("Condition is false!");
+}
+// Output: condition is true
+
+if(numOne === numTwo) {
+    console.log("Condition is true!");
+}
+else {
+    console.log("Condition is false!");
+}
+// Output: condition is false
