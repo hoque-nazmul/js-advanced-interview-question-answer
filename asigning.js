8870087d21c9c2d0babf42a63957430ccc836cf7
Nazmul Hoque <coder.nazmul@gmail.com>
2020-05-17 04:30:58 +0600

    modified

--- asigning.js
+++ asigning.js
@@ -6,10 +6,13 @@ console.log(b);
 // b = 5 
 // a is not defined..
 
-var output = (function(x) {
+var output = (function (x) {
     delete x;
     return x;
-  })(0);
-  
-  console.log(output);
-  // output: 0
+})(0);
+
+console.log(output);
+// output: 0
+
+console.log(0.1 + 0.2 === 0.3);
+// output: false;
