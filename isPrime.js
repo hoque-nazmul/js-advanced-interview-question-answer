05c8adf715fc9a4713e9e52c5a8004388f76d9b9
Nazmul Hoque <coder.nazmul@gmail.com>
2020-05-17 13:43:55 +0600

    prime number checked

---
+++ isPrime.js
@@ -0,0 +1,13 @@
+const isPrime = (num) => {
+    let divisor = 2;
+    while (num > divisor) {
+        if (num % divisor === 0) {
+            return false;
+        } else {
+            divisor++;
+        }
+    }
+    return true;
+}
+console.log(isPrime(7));
+console.log(isPrime(9));
